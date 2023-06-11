a493a81550d8c2dbe4e3c2f32e47513db52d8515
cbusquets <busquetschristian30@gmail.com>
2023-06-11 19:06:39 -0300

    Fix Controller usuario y agrege desctructor a ControllerConvMensj

--- InterfaceConvMens.h
+++ InterfaceConvMens.h
@@ -18,18 +18,19 @@ class InterfaceConvMens
 	public:
 		virtual set<DtConversacionGrupal> mostrarConverG() = 0;
 		virtual set<DtConversacionPrivada> mostrarConverP()=0;
-        virtual int mostrarCantidad() = 0;
-        virtual set<DtConversacion> seleccionarConversacion(string id) = 0;
-        virtual set<DtMensaje> datosMensajes(string id) = 0;
-        virtual DtMensaje borrarMensaje(DtConversacion borrar) = 0;
-        virtual set<DtConversacion> seleccionarConversacionesTodas() = 0;
-        virtual DtConversacion ingresarIDConversacion(int idConve) = 0;
-        virtual DtMensaje enviarMsjSimple(string texto) = 0;
-        virtual DtMensaje enviarMsjVideo(string url, string formato, float duracion) = 0;
-        virtual DtMensaje enviarMsjCompartirContacto(int celularCompContacto) = 0;
-        virtual DtMensaje enviarMsjImagen(string url, string tamanio, string formato) = 0;
-        virtual set<DtConversacion> buscarConv(int id) = 0;
-        virtual set<DtConversacion> buscarConvArch(int id) = 0;
-        virtual DtMensaje seleccionarMsj(int id) = 0;
+        virtual int mostrarCantidad()=0;
+        virtual set<DtConversacion> seleccionarConversacion(string id)=0;
+        virtual set<DtMensaje> datosMensajes(string id)=0;
+        virtual  DtMensaje borrarMensaje(DtConversacion borrar)=0;
+        virtual set<DtConversacion> seleccionarConversacionesTodas()=0;
+        virtual DtConversacion ingresarIDCOnversacion(int idConve)=0;
+        virtual DtMensaje enviarMsjSimple(string texto)=0;
+        virtual DtMensaje enviarMsjVideo(string url, string formato, float duracion)=0;
+        virtual DtMensaje enviarMsjCompartirContacto(int celularCompContacto)=0;
+        virtual DtMensaje enviarMsjImagen(string url, string tamanio, string formato)=0;
+        virtual set<DtConversacion> buscarConv(int id)=0;
+        virtual set<DtConversacion> buscarConvArch(int id)=0;
+        virtual DtMensaje seleccionarMsj(int id)=0;
+        virtual ~InterfaceConvMens();
 };
 #endif
